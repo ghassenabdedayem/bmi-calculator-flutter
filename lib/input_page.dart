1ca5cec878cf29ad49315f85779ab384a8e78c4c
ghassenabdedayem <ghassen.abdedayem@gmail.com>
2020-06-04 04:10:15 +0200

    Card containers and global theme with code refactoring

---
+++ lib/input_page.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+
+const bottomContainerHeight = 80.0;
+const activeCardColor = Color(0xFF1D1E33);
+const bottomContainerColor = Color(0xFFEB1555);
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI CALCULATOR'),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: ReusableCard(colour: activeCardColor),
+                ),
+                Expanded(
+                  child: ReusableCard(colour: activeCardColor),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ReusableCard(colour: activeCardColor),
+          ),
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: ReusableCard(colour: activeCardColor),
+                ),
+                Expanded(
+                  child: ReusableCard(colour: activeCardColor),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            color: bottomContainerColor,
+            width: double.infinity,
+            margin: EdgeInsets.only(top: 10.0),
+            height: bottomContainerHeight,
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+//  const ReusableCard({
+//    Key key,
+//  }) : super(key: key);
+// we don't need this constructor, because the key is used to
+// track the state of widghets tha are moving in the screen
+  ReusableCard({@required this.colour, this.cardChild});
+
+  final Color colour; // final makes this property immutable
+  final Widget cardChild;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: cardChild,
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        color: colour,
+      ),
+    );
+  }
+}
+
+//      floatingActionButton: Theme(
+//        data: ThemeData(accentColor: Colors.purple),
+//        child: FloatingActionButton(
+//          child: Icon(Icons.add),
+//        ),
+//      ),
